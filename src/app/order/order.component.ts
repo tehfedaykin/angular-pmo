56317d6089e8d8cda8aea20d986f08672b147339
tehfedaykin <jennifer.wadella@gmail.com>
2019-01-29 11:24:36 -0600

    i have no idea

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
