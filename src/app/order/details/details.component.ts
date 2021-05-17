1f41e12fb142704e1860b852a63ad16ff7206737
Kyle Nazario <knazario@bitovi.com>
2021-05-17 10:54:23 -0600

    Order details handles no order provided

--- src/app/order/details/details.component.ts
+++ src/app/order/details/details.component.ts
@@ -7,11 +7,15 @@ import { Order, OrderService } from '../order.service';
   styleUrls: ['./details.component.css']
 })
 export class OrderDetailsComponent implements OnInit {
-  @Input() order: Order;
+  @Input() order?: Order;
   orderTotal: number = 0.0;
   constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+    if (!this.order) {
+      throw new Error('Order is not defined');
+    }
+
     this.orderTotal = this.orderService.getTotal(this.order.items);
   }
 }
