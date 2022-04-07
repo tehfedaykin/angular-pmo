2ea0afa89feda24301dff419eac5799b11c1c56f
Wade H <wade@bitovi.com>
2022-04-07 13:12:16 -0400

    refactor: move states subscription callback into pipe

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import {Subject} from 'rxjs';
+import { Subject } from 'rxjs';
 
 import { RestaurantService, Config, City, State } from './restaurant.service';
 import { Restaurant } from './restaurant';
-import {takeUntil} from "rxjs/operators";
+import { takeUntil, tap } from 'rxjs/operators';
 
 export interface Data<T> {
   value: Array<T>;
@@ -14,23 +14,23 @@ export interface Data<T> {
 @Component({
   selector: 'pmo-restaurant',
   templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.less']
+  styleUrls: ['./restaurant.component.less'],
 })
 export class RestaurantComponent implements OnInit, OnDestroy {
   form!: FormGroup;
 
   public restaurants: Data<Restaurant> = {
     value: [],
-    isPending: false
+    isPending: false,
   };
   public states: Data<State> = {
     value: [],
-    isPending: true
+    isPending: true,
   };
 
   public cities: Data<City> = {
     value: [],
-    isPending: true
+    isPending: true,
   };
 
   private unSubscribe = new Subject<void>();
@@ -38,28 +38,29 @@ export class RestaurantComponent implements OnInit, OnDestroy {
   constructor(
     private restaurantService: RestaurantService,
     private fb: FormBuilder
-    ) {
-  }
+  ) { }
 
   ngOnInit() {
     this.createForm();
     this.states.isPending = true;
-    this.restaurantService.getStates().subscribe((res: Config<State>) => {
-      this.states.value = res.data;
-      this.states.isPending = false;
-      this.form.get('state')?.enable();
-    });
+    this.restaurantService.getStates().pipe(
+      takeUntil(this.unSubscribe),
+      tap((res: Config<State>) => {
+        this.states.value = res.data;
+        this.states.isPending = false;
+        this.form.get('states')?.enable();
+      }));
   }
 
   ngOnDestroy(): void {
-    this.unSubscribe.next()
+    this.unSubscribe.next();
     this.unSubscribe.complete();
   }
 
   createForm() {
     this.form = this.fb.group({
-      state: {value: '', disabled: true},
-      city: {value: '', disabled: true},
+      state: { value: '', disabled: true },
+      city: { value: '', disabled: true },
     });
 
     this.onChanges();
@@ -67,35 +68,41 @@ export class RestaurantComponent implements OnInit, OnDestroy {
 
   onChanges(): void {
     let state: string;
-    this.form.get('state')?.valueChanges.pipe(takeUntil(this.unSubscribe)).subscribe(val => {
-      console.log('state', state, val);
-      if (val) {
-        this.form.get('city')?.enable({
-          onlySelf: true,
-          emitEvent: false
-        });
-        // eslint-disable-next-line eqeqeq
-        if (state != val) {
-          this.form.get('city')?.patchValue('');
+    this.form
+      .get('state')
+      ?.valueChanges.pipe(takeUntil(this.unSubscribe))
+      .subscribe((val) => {
+        console.log('state', state, val);
+        if (val) {
+          this.form.get('city')?.enable({
+            onlySelf: true,
+            emitEvent: false,
+          });
+          // eslint-disable-next-line eqeqeq
+          if (state != val) {
+            this.form.get('city')?.patchValue('');
+            this.restaurants.value = [];
+          }
+          this.getCities(val);
+          state = val;
+        } else {
+          this.form.get('city')?.disable({
+            onlySelf: true,
+            emitEvent: false,
+          });
+          state = '';
           this.restaurants.value = [];
         }
-        this.getCities(val);
-        state = val;
-      } else {
-        this.form.get('city')?.disable({
-          onlySelf: true,
-          emitEvent: false
-        });
-        state = '';
-        this.restaurants.value = [];
-      }
-    });
+      });
 
-    this.form.get('city')?.valueChanges.pipe(takeUntil(this.unSubscribe)).subscribe(val => {
-      if (val) {
-        this.getRestaurants(state, val);
-      }
-    });
+    this.form
+      .get('city')
+      ?.valueChanges.pipe(takeUntil(this.unSubscribe))
+      .subscribe((val) => {
+        if (val) {
+          this.getRestaurants(state, val);
+        }
+      });
   }
 
   getCities(state: string) {
@@ -105,16 +112,17 @@ export class RestaurantComponent implements OnInit, OnDestroy {
       this.cities.isPending = false;
       this.form.get('city')?.enable({
         onlySelf: true,
-        emitEvent: false
+        emitEvent: false,
       });
     });
   }
 
   getRestaurants(state: string, city: string) {
-    this.restaurantService.getRestaurants(state, city).subscribe((res: Config<Restaurant>) => {
-      this.restaurants.value = res.data;
-      this.restaurants.isPending = false;
-    });
+    this.restaurantService
+      .getRestaurants(state, city)
+      .subscribe((res: Config<Restaurant>) => {
+        this.restaurants.value = res.data;
+        this.restaurants.isPending = false;
+      });
   }
-
 }
